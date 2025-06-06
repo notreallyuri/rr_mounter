5750da91676b38d23df4a08bfee2527b12fac806
YuriVGR <yurivgr@icloud.com>
2025-06-06 03:16:03 -0300

    Removed unused metadata, moved to a static JSON file

--- src/base/source_info.rs
+++ src/base/source_info.rs
@@ -1,15 +1,4 @@
-use bitflags::bitflags;
 use serde::{Deserialize, Serialize};
-
-bitflags! {
-    #[derive(Debug, Clone, Serialize, Deserialize)]
-    pub struct SourceIntents: u32 {
-        const MANGA_CHAPTERS = 1<<0;
-        const MANGA_TRACKING = 1<<1;
-        const HOMEPAGE_SECTIONS = 1<<2;
-        const CLOADFARE_BYPASS_REQUIRED = 1<<3;
-    }
-}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
